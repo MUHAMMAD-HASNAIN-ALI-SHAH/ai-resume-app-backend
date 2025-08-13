9657939c760a64866813e3eef8a80876008d611c
M_HASNAIN_ALI_SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-08-13 15:02:01 +0500

    get resume by id

--- controllers/create.resume.route.js
+++ controllers/create.resume.route.js
@@ -45,6 +45,26 @@ const getMyResumes = async (req, res) => {
     console.error("Error fetching resumes:", error);
     res.status(500).json({ message: "Internal server error" });
   }
-}
+};
 
-module.exports = { CreateResume, getMyResumes };
+const getMyResumesById = async (req, res) => {
+  try {
+    const { resumeId } = req.params;
+    const { email } = req.session.user;
+    if (!email) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const resume = await Resume.findOne({ _id: resumeId, userEmail: email });
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    return res.status(200).json(resume);
+  } catch (error) {
+    console.error("Error fetching resume by ID:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = { CreateResume, getMyResumes, getMyResumesById };
--- routes/create.resume.route.js
+++ routes/create.resume.route.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   CreateResume,
   getMyResumes,
+  getMyResumesById,
 } = require("../controllers/create.resume.route");
 const router = express.Router();
 
 router.route("/").post(CreateResume);
 router.route("/").get(getMyResumes);
+router.route("/:resumeId").get(getMyResumesById);
 
 module.exports = router;
